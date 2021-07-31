c94b8735900505b190a5f43bc54586edeaa666a4
栗田大輔 <daikons.msn36@gmail.com>
2021-08-01 02:11:41 +0900

    初めてのコミット

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2021_07_31_094712) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+  end
+
+end
